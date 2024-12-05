5b8d6af0fa73da599c24bfa5a7d25be37655e275
mohamed amamra <matrixrunner1@outlook.com>
2024-12-05 12:47:51 +0100

    third commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,6 +3,8 @@ import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
 
+
+
 export default function Navbar() {
   return (
     <nav className=" mb-20 flex items-center justify-between py-6">
