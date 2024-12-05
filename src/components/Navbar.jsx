43bbd9f1b132ecc3d3fa8c5d75af8ede61fbed43
mohamed amamra <matrixrunner1@outlook.com>
2024-12-05 14:19:42 +0100

    linkdin url

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ export default function Navbar() {
             <h1 className="mx-4 text-3xl hover:text-purple-400 transition-all ease-in delay-100">Mhd</h1>
         </div>
         <div className="m-8 flex items-center justify-center gap-4 text-2xl ">
-            <a href="www.linkedin.com/in/mohamed-amamra-336461324" target='_blank'><FaLinkedin className='hover:text-purple-400 transition-all ease-in delay-100 cursor-pointer' /></a>
+            <a href="https://www.linkedin.com/in/mohamed-amamra-336461324" target='_blank'><FaLinkedin className='hover:text-purple-400 transition-all ease-in delay-100 cursor-pointer' /></a>
             <a href="https://github.com/mhdrxnr" target='_blank'><FaGithub className='hover:text-purple-400 transition-all ease-in delay-100 cursor-pointer'/></a>
             <a href="https://x.com/Mhd__offc" target='_blank'><FaSquareXTwitter className='hover:text-purple-400 transition-all ease-in delay-100 cursor-pointer'/></a>
             <a href="https://instagram.com/mhd__offc" target='_blank'><FaInstagram className='hover:text-purple-400 transition-all ease-in delay-100 cursor-pointer'/></a>
